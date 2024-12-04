36ba9cde932841d5e6ae8eb5ffba3ff9fe092bd1
nadyozha <alexandrsnadyozha@gmail.com>
2024-12-04 18:37:33 +0100

    fixed http request

--- src/useKey.js
+++ src/useKey.js
@@ -1,14 +1,14 @@
 import { useEffect } from "react";
 
 export default function useKey(action, key) {
-    useEffect(function () {
-        function keypress(e) {
-            if (e.code.toLowerCase() === key.toLowerCase()) action();
-        }
-        document.addEventListener('keydown', keypress);
+	useEffect(function () {
+		function keypress(e) {
+			if (e.code.toLowerCase() === key.toLowerCase()) action();
+		}
+		document.addEventListener('keydown', keypress);
 
-        return function () {
-            document.removeEventListener('keydown', keypress);
-        }
-    }, [action, key]);
+		return function () {
+			document.removeEventListener('keydown', keypress);
+		}
+	}, [action, key]);
 }
--- src/useMovies.js
+++ src/useMovies.js
@@ -2,45 +2,46 @@ import { useEffect, useState } from "react";
 import { APIKEY } from "./APIKEY";
 
 export default function useMovies(query) {
-    const [movies, setMovies] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState('');
+	const [movies, setMovies] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+	const [error, setError] = useState('');
 
-    useEffect(function () {
-        //callback?.();
-        const controller = new AbortController();
-        async function fetchMovies() {
-            try {
-                setIsLoading(true);
-                setError('');
+	useEffect(function () {
+		//callback?.();
+		const controller = new AbortController();
+		async function fetchMovies() {
+			try {
+				setIsLoading(true);
+				setError('');
 
-                const res = await fetch(`http://www.omdbapi.com/?apikey=${APIKEY}&s=${query}`, { signal: controller.signal });
 
-                if (!res.ok) throw new Error('Smth went wrong with fetching movies');
-                const data = await res.json();
-                if (data.Response === "False") throw new Error('Movie not found');
+				const res = await fetch(`https://www.omdbapi.com/?apikey=${APIKEY}&s=${query}`, { signal: controller.signal });
 
-                setMovies(data.Search || []);
-                setError('');
-            } catch (err) {
-                if (err.name !== 'AbortError') setError(err.message);
-            } finally {
-                setIsLoading(false);
-            }
-        }
+				if (!res.ok) throw new Error('Smth went wrong with fetching movies');
+				const data = await res.json();
+				if (data.Response === "False") throw new Error('Movie not found');
 
-        if (query.length < 3) {
-            setMovies([]);
-            setError('');
-            return
-        }
+				setMovies(data.Search || []);
+				setError('');
+			} catch (err) {
+				if (err.name !== 'AbortError') setError(err.message);
+			} finally {
+				setIsLoading(false);
+			}
+		}
 
-        fetchMovies();
+		if (query.length < 3) {
+			setMovies([]);
+			setError('');
+			return
+		}
 
-        return function () {
-            controller.abort();
-        }
-    }, [query]);
+		fetchMovies();
 
-    return { movies, isLoading, error };
+		return function () {
+			controller.abort();
+		}
+	}, [query]);
+
+	return { movies, isLoading, error };
 }
